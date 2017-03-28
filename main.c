7b583d1e29b656ba9a45ee4ce0a766431ea469e7
Michael Kemp <mkem114@aucklanduni.ac.nz>
2017-03-29 00:53:49 +1300

    I lied; a tale of formatting
    My fresh IDE install didn't format the way I liked

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
-{
+int main () {
     long long int toTest = 600851475143;
     long long int currentDivisor = 2;
 
-    while (toTest > currentDivisor)
-    {
-        if (toTest % currentDivisor==0) {
+    while (toTest > currentDivisor) {
+        if (toTest % currentDivisor == 0) {
             toTest = toTest / currentDivisor;
             currentDivisor = 2;
         }
